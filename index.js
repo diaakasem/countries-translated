7e262cc2f9f3ce0194dab056762326ee84d4c8c6
Diaa Kasem <me@diaa.me>
2018-01-03 16:17:40 +0200

    Update ountries list and generator and use auto version

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const _ = require('lodash');
 let countries = require('./countries.json');
 const fs = require('fs');
 
+function processCountry(country, key) {
+    return translate(country.name, {to: 'ar'}).then((res)=> {
+        if (!country.translations) {
+            country.translations = {};
+        }
+        country.translations.en = country.name;
+        country.translations.ar = res.text;
+        return country
+    }).catch(err => {
+        console.error(err);
+        return err;
+    });
+}
 
 //countries = {'AD': countries.AD};
-Promise.all(
-    _.map(countries, (country, key)=> {
-        return translate(country.name, {to: 'ar'}).then((res)=> {
-            if (!country.translations) {
-                country.translations = {};
-            }
-            country.translations.ar = res.text;
-            return country
-        }).catch(err => {
-            console.error(err);
-        });
-    })
-).then((allCountries)=> {
+let promises = _.map(countries, processCountry);
+Promise.all(promises).then((allCountries)=> {
     let json = JSON.stringify(allCountries, null, 2);
-    fs.writeFile('countries-list.json', json);    
+    console.log(json);
+    return fs.writeFileSync('countries-list.json', json);
+}).catch((err)=> {
+    console.error(err);
 });
